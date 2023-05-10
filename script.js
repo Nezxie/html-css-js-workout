ec5d0e5af2cd312c4656d2d557d57ded838fd909
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2023-05-10 04:52:02 +0200

    add changing colors

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello from Nerdbord!')
+function changeColor(color,elem){
+    elem.style['border-color']=color;
+}
+let box = document.querySelector('.box');
+let colors = ['red','magenta','orange','blue'];
+let position = 0;
+window.setInterval(function(){
+        changeColor(colors[position],box);
+        if(position>3){
+            position=0;
+        }
+        else{
+            position++
+        }
+    }
+    ,3000);
